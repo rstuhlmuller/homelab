04403f20cbc150327205460be87306b51fdef333
Rodman <rodman@stuhlmuller.net>
2025-05-01 20:15:59 -0700

    fix: stuff

--- IaC/modules/argocd/main.tf
+++ IaC/modules/argocd/main.tf
@@ -2,7 +2,7 @@ resource "helm_release" "argocd" {
   name             = var.name
   chart            = var.chart
   repository       = var.repository
-  version          = var.version
+  version          = var.chart_version
   timeout          = var.timeout
   namespace        = var.namespace
   create_namespace = var.create_namespace
