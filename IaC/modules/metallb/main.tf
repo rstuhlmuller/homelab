4f859f654673ef7de32c6dc052a652c06213d681
Rodman <rodman@stuhlmuller.net>
2025-05-08 19:09:36 -0700

    fix: missing ip address pool

--- IaC/modules/metallb/main.tf
+++ IaC/modules/metallb/main.tf
@@ -9,3 +9,33 @@ resource "kubernetes_namespace" "metallb_system" {
     }
   }
 }
+
+resource "kubernetes_manifest" "ip_address_pool" {
+  manifest = {
+    apiVersion = "metallb.io/v1beta1"
+    kind       = "IPAddressPool"
+    metadata = {
+      name      = "first-pool"
+      namespace = kubernetes_namespace.metallb_system.metadata[0].name
+    }
+    spec = {
+      addresses = [
+        "10.1.0.128/25"
+      ]
+    }
+  }
+}
+
+resource "kubernetes_manifest" "l2_advertisement" {
+  manifest = {
+    apiVersion = "metallb.io/v1beta1"
+    kind       = "L2Advertisement"
+    metadata = {
+      name      = "l2-advertisement"
+      namespace = kubernetes_namespace.metallb_system.metadata[0].name
+    }
+    spec = {}
+  }
+
+  depends_on = [kubernetes_manifest.ip_address_pool]
+}
