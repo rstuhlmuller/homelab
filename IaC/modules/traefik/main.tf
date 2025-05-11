77aac5b10121614e5fc2f892fd533a485233cd41
Rodman <rodman@stuhlmuller.net>
2025-05-10 17:22:05 -0700

    feat: traefik

--- IaC/modules/traefik/main.tf
+++ IaC/modules/traefik/main.tf
@@ -3,3 +3,25 @@ resource "kubernetes_namespace" "traefik" {
     name = "traefik"
   }
 }
+
+resource "helm_release" "traefik" {
+  name       = "traefik"
+  repository = "https://helm.traefik.io/traefik"
+  chart      = "traefik"
+  namespace  = kubernetes_namespace.traefik.metadata[0].name
+
+  set {
+    name  = "service.type"
+    value = "LoadBalancer"
+  }
+
+  set {
+    name  = "ingressRoute.dashboard.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "ingressRoute.dashboard.entryPoints"
+    value = "{web}"
+  }
+}
