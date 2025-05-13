40d7d8c556ac8a85b391a48f8993c155d2c461f2
Rodman <rodman@stuhlmuller.net>
2025-05-12 20:49:33 -0700

    feat: prometheus

---
+++ IaC/modules/monitoring/main.tf
@@ -0,0 +1,55 @@
+resource "kubernetes_namespace" "monitoring" {
+  metadata {
+    name = "monitoring"
+    labels = {
+      "pod-security.kubernetes.io/enforce" = "privileged",
+      "pod-security.kubernetes.io/audit"   = "privileged",
+      "pod-security.kubernetes.io/warn"    = "privileged"
+    }
+  }
+}
+
+resource "argocd_application" "prometheus" {
+  metadata {
+    name = "monitoring"
+  }
+
+  spec {
+    project = "default"
+    destination {
+      server    = "https://kubernetes.default.svc"
+      namespace = kubernetes_namespace.monitoring.metadata[0].name
+    }
+
+    source {
+      repo_url        = "https://prometheus-community.github.io/helm-charts"
+      chart           = "prometheus"
+      target_revision = "27.13.0"
+      helm {
+        value_files = ["values.yaml"]
+        values = yamlencode({
+          server = {
+            ingress = {
+              enabled          = true
+              ingressClassName = "traefik"
+              tls = [
+                {
+                  secret_name = "prometheus-certificate-secret"
+                  hosts = [
+                    "prometheus.stinkyboi.com"
+                  ]
+                }
+              ]
+            }
+          }
+        })
+      }
+    }
+    sync_policy {
+      automated {
+        prune     = true
+        self_heal = true
+      }
+    }
+  }
+}
---
+++ IaC/production/homelab/monitoring/.terraform.lock.hcl
@@ -0,0 +1,1 @@
+../../../_envcommon/locks/monitoring.terraform.lock.hcl
