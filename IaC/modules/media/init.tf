26d17388df6c5eb9a6e4f49fd451ad2d23884eda
Rodman <rodman@stuhlmuller.net>
2025-05-24 15:27:03 -0700

    feat: media stuff (#4)

---
+++ IaC/modules/media/init.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.36"
+    }
+    argocd = {
+      source  = "argoproj-labs/argocd"
+      version = "~> 7.7"
+    }
+  }
+}
+locals {
+  tags = merge(
+    # var.tags,
+    {
+      Module = "self/monitoring"
+    }
+  )
+}
---
+++ IaC/production/homelab/media/terragrunt.hcl
@@ -0,0 +1,1 @@
+../../../_envcommon/media.hcl
