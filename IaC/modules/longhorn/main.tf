9fdcb206bc340d067e49e897091ba83196fdde77
Rodman <rodman@stuhlmuller.net>
2025-05-28 22:19:38 -0700

    feat: bring back longhorn

---
+++ IaC/modules/longhorn/main.tf
@@ -0,0 +1,44 @@
+resource "kubernetes_namespace" "longhorn_system" {
+  metadata {
+    name = "longhorn-system"
+
+    labels = {
+      "pod-security.kubernetes.io/enforce" = "privileged"
+      "pod-security.kubernetes.io/audit"   = "privileged"
+      "pod-security.kubernetes.io/warn"    = "privileged"
+    }
+  }
+}
+
+resource "helm_release" "longhorn" {
+  name       = "longhorn"
+  repository = "https://charts.longhorn.io"
+  chart      = "longhorn"
+  namespace  = kubernetes_namespace.longhorn_system.metadata[0].name
+
+  # Add CRD management settings
+  dependency_update = true
+  force_update      = true
+  cleanup_on_fail   = true
+  atomic            = true
+
+  set {
+    name  = "defaultSettings.defaultDataLocality"
+    value = "best-effort"
+  }
+
+  set {
+    name  = "defaultSettings.defaultReplicaAutoBalance"
+    value = "true"
+  }
+
+  set {
+    name  = "defaultSettings.defaultAllowVolumeExpansion"
+    value = "true"
+  }
+
+  set {
+    name  = "persistence.defaultClass"
+    value = "false"
+  }
+}
