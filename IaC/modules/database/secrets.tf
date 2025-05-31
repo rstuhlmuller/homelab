e18beff946f5fe88da3cae72d9be77327f08ea87
Rodman <rodman@stuhlmuller.net>
2025-05-31 09:14:54 -0700

    fix: postgreqsl (#16)

---
+++ IaC/modules/database/secrets.tf
@@ -0,0 +1,41 @@
+locals {
+  secrets = [
+    "postgres-password",
+    "password",
+    "replication-password"
+  ]
+}
+resource "aws_ssm_parameter" "postgresql_secret" {
+  #checkov:skip=CKV_AWS_337: Need to update with project key
+  for_each = toset(local.secrets)
+  name     = "/homelab/${kubernetes_namespace.postgresql.metadata[0].name}/${each.key}"
+  type     = "SecureString"
+  value    = "update_me"
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "kubernetes_manifest" "postgresql_secret" {
+  manifest = {
+    apiVersion = "external-secrets.io/v1"
+    kind       = "ExternalSecret"
+    metadata = {
+      name      = "postgresql-secret"
+      namespace = kubernetes_namespace.postgresql.metadata[0].name
+    }
+    spec = {
+      secretStoreRef = {
+        name = "parameterstore"
+        kind = "ClusterSecretStore"
+      }
+      refreshInterval = "30s"
+      data = [for k, v in aws_ssm_parameter.postgresql_secret : {
+        secretKey = k
+        remoteRef = {
+          key = v.name
+        }
+      }]
+    }
+  }
+}
