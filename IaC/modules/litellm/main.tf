7c4ac61c9a0af5e0012e8a0b4ac4c53d3e337e3a
Rodman <rodman@stuhlmuller.net>
2025-08-07 22:49:37 -0700

    feat: litellm

--- IaC/modules/litellm/main.tf
+++ IaC/modules/litellm/main.tf
@@ -25,15 +25,74 @@ resource "argocd_application" "litellm" {
       path            = "deploy/charts/litellm-helm"
       target_revision = "HEAD"
       helm {
-        parameter {
-          name  = "migrationJob.enabled"
-          value = "false"
-        }
-        parameter {
-          name  = "db.useExisting"
-          value = "true"
-        }
         values = yamlencode({
+          migrationJob = {
+            enabled = false
+          }
+          image = {
+            repository = "ghcr.io/berriai/litellm-non_root"
+            pullPolicy = "Always"
+            tag        = "main-v1.65.3-nightly"
+          }
+          db = {
+            deployStandalone = false
+            useExisting      = true
+            endpoint         = "postgresql.database.svc.cluster.local:5432"
+          }
+          ingress = {
+            enabled   = true
+            className = "traefik"
+            hosts = [
+              {
+                host = "litellm.stinkyboi.com"
+                paths = [
+                  {
+                    path     = "/"
+                    pathType = "ImplementationSpecific"
+                  }
+                ]
+              }
+            ]
+            tls = [
+              {
+                secretName = "litellm-tls"
+                hosts      = ["litellm"]
+              }
+            ]
+          }
+          environmentSecrets = ["litellm-env"]
+          proxy_config = {
+            model_list = [
+              {
+                model_name = "Nova Pro"
+                litellm_params = {
+                  model           = "bedrock/amazon.nova-pro-v1:0"
+                  aws_region_name = "us-west-2"
+                }
+              },
+              {
+                model_name = "Claude Sonnet 4"
+                litellm_params = {
+                  model           = "bedrock/us.anthropic.claude-sonnet-4-20250514-v1:0"
+                  aws_region_name = "us-west-2"
+                }
+              },
+              {
+                model_name = "DeepSeek-R1"
+                litellm_params = {
+                  model           = "bedrock/converse/deepseek.r1-v1:0"
+                  aws_region_name = "us-west-2"
+                }
+              },
+              {
+                model_name = "gpt-oss-120b"
+                litellm_params = {
+                  model           = "bedrock/openai.gpt-oss-120b-1:0"
+                  aws_region_name = "us-west-2"
+                }
+              }
+            ]
+          }
         })
       }
     }
--- IaC/modules/litellm/secrets.tf
+++ IaC/modules/litellm/secrets.tf
@@ -1,36 +1,71 @@
-# resource "aws_ssm_parameter" "open_webui" {
-#   for_each = toset(["openai_api_key"])
-#   #checkov:skip=CKV_AWS_337: Need to update with project key
-#   name        = "/homelab/${kubernetes_namespace.open_webui.metadata[0].name}/${each.key}"
-#   description = "Secret for Open WebUI"
-#   type        = "SecureString"
-#   value       = "update_me"
-#   lifecycle {
-#     ignore_changes = [value]
-#   }
-# }
+resource "aws_ssm_parameter" "litellm" {
+  for_each = toset(["username", "password"])
+  #checkov:skip=CKV_AWS_337: Need to update with project key
+  name        = "/homelab/${kubernetes_namespace.litellm.metadata[0].name}/database/${each.key}"
+  description = "Secret for Open WebUI"
+  type        = "SecureString"
+  value       = "update_me"
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
 
-# resource "kubernetes_manifest" "open_webui_secret" {
-#   manifest = {
-#     apiVersion = "external-secrets.io/v1"
-#     kind       = "ExternalSecret"
-#     metadata = {
-#       name      = "open-webui-secret"
-#       namespace = kubernetes_namespace.open_webui.metadata[0].name
-#     }
-#     spec = {
-#       secretStoreRef = {
-#         name = "parameterstore"
-#         kind = "ClusterSecretStore"
-#       }
-#       refreshPolicy   = "Periodic"
-#       refreshInterval = "10m"
-#       data = [for key, value in aws_ssm_parameter.open_webui : {
-#         secretKey = key
-#         remoteRef = {
-#           key = value.name
-#         }
-#       }]
-#     }
-#   }
-# }
+resource "kubernetes_manifest" "litellm_secret" {
+  manifest = {
+    apiVersion = "external-secrets.io/v1"
+    kind       = "ExternalSecret"
+    metadata = {
+      name      = "postgres"
+      namespace = kubernetes_namespace.litellm.metadata[0].name
+    }
+    spec = {
+      secretStoreRef = {
+        name = "parameterstore"
+        kind = "ClusterSecretStore"
+      }
+      refreshPolicy = "OnChange"
+      data = [for key, value in aws_ssm_parameter.litellm : {
+        secretKey = key
+        remoteRef = {
+          key = value.name
+        }
+      }]
+    }
+  }
+}
+
+resource "aws_ssm_parameter" "litellm_env" {
+  for_each = toset(["AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY", "AWS_REGION_NAME"])
+  #checkov:skip=CKV_AWS_337: Need to update with project key
+  name        = "/homelab/${kubernetes_namespace.litellm.metadata[0].name}/env/${each.key}"
+  description = "Secret for Open WebUI"
+  type        = "SecureString"
+  value       = "update_me"
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "kubernetes_manifest" "litellm_env_secret" {
+  manifest = {
+    apiVersion = "external-secrets.io/v1"
+    kind       = "ExternalSecret"
+    metadata = {
+      name      = "litellm-env"
+      namespace = kubernetes_namespace.litellm.metadata[0].name
+    }
+    spec = {
+      secretStoreRef = {
+        name = "parameterstore"
+        kind = "ClusterSecretStore"
+      }
+      refreshPolicy = "OnChange"
+      data = [for key, value in aws_ssm_parameter.litellm_env : {
+        secretKey = key
+        remoteRef = {
+          key = value.name
+        }
+      }]
+    }
+  }
+}
