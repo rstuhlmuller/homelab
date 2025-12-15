83314eb58099093765d9349e0ec3bffbe2a50faf
Rodman <rodman@stuhlmuller.net>
2025-12-14 16:43:23 -0800

    feat: minecraft (#94)

---
+++ IaC/modules/minecraft/versions.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.36"
+    }
+    argocd = {
+      source  = "argoproj-labs/argocd"
+      version = "7.11.2"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+locals {
+  tags = merge(
+    var.tags,
+    {
+      Module = "self/minecraft"
+    }
+  )
+}
