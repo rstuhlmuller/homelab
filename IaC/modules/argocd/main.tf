1db7e7aaf9a6646467d70e29ff758e72a195bc34
Rodman <rodman@stuhlmuller.net>
2025-12-23 22:00:20 -0800

    fix: bad merge

--- IaC/modules/argocd/main.tf
+++ IaC/modules/argocd/main.tf
@@ -110,7 +110,7 @@ resource "helm_release" "argocd_image_updater" {
   name       = "argocd-image-updater"
   chart      = "argocd-image-updater"
   repository = "https://argoproj.github.io/argo-helm"
-  version    = "1.0.2"
+  version    = "1.0.4"
   namespace  = kubernetes_namespace_v1.argocd.metadata[0].name
   wait       = true
 }
